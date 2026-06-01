307b024c6dc1b5ff9d2ecfd61781d31c51af8c26
bkv <a@bkv.me>
2026-06-01 22:23:41 +0200

    [UPDATE] upgrading ci packages

--- crates/api/src/openapi.rs
+++ crates/api/src/openapi.rs
@@ -18,7 +18,7 @@ use crate::problem::{FieldErrorView, Problem};
 #[openapi(
     info(
         title = "IntelliPilot API",
-        version = "0.1.10",
+        version = "0.1.11",
         description = "Lightweight project management API.",
         contact(
             name = "IntelliSoftAlpin eG",
