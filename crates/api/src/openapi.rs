e3731ccdc06788c5c93fd9eb227b1b6ebeedc4b7
bkv <a@bkv.me>
2026-06-05 17:03:14 +0200

    [FIX] aligning versions

--- crates/api/src/openapi.rs
+++ crates/api/src/openapi.rs
@@ -18,7 +18,7 @@ use crate::problem::{FieldErrorView, Problem};
 #[openapi(
     info(
         title = "IntelliPilot API",
-        version = "0.1.15",
+        version = "0.1.17",
         description = "Lightweight project management API.",
         contact(
             name = "IntelliSoftAlpin eG",
