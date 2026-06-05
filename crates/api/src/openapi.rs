a4b0d6a96a20136662e45b9ac0a272204e878682
bkv <a@bkv.me>
2026-06-05 23:00:03 +0200

    [FIX] align back and front

--- crates/api/src/openapi.rs
+++ crates/api/src/openapi.rs
@@ -18,7 +18,7 @@ use crate::problem::{FieldErrorView, Problem};
 #[openapi(
     info(
         title = "IntelliPilot API",
-        version = "0.1.17",
+        version = "0.2.1",
         description = "Lightweight project management API.",
         contact(
             name = "IntelliSoftAlpin eG",
